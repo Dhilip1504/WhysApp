901eb1db7937b6a9d7a30cb821b3704e62c763ff
Dhilip1504 <dhilip1504@gmail.com>
2020-07-26 19:36:47 +0530

    Initial commit

---
+++ app/src/main/java/com/team154/whysapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.team154.whysapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
